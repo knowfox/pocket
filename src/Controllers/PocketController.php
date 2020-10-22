2b311acc6a73abb3be535e638373f91add401ad0
Olav Schettler <olav.schettler@dw.com>
2020-10-22 23:11:15 +0200

    Using djchen/pocket-api-php

--- src/Controllers/PocketController.php
+++ src/Controllers/PocketController.php
@@ -3,8 +3,7 @@
 namespace Knowfox\Pocket\Controllers;
 
 use Illuminate\Http\Request;
-use Duellsy\Pockpack\Pockpack;
-use Duellsy\Pockpack\PockpackAuth;
+use Pocket\Pocket as PocketApi;
 use Knowfox\Pocket\Models\Pocket;
 use Knowfox\Core\Models\Concept;
 use Carbon\Carbon;
@@ -21,23 +20,31 @@ class PocketController
         if ($pocket) {
             $today = Carbon::today()->getTimestamp();
 
-            $pockpack = new Pockpack($consumer_key, $pocket->access_token);
-            $list = $pockpack->retrieve([
+            $api = new PocketApi([
+                'consumerKey' => $consumer_key
+            ]);
+            $api->setAccessToken($pocket->access_token);
+
+            $list = $api->retrieve([
                 'state' => 'all',
                 'since' => $today,
                 'detailType' => 'simple',
-            ], /*as_array*/true);
+            ]);
             if ($list) {
                 $invalid_token = false;
             }
         }
 
         if ($invalid_token) {
-            $pockpack = new PockpackAuth();
-            $request_token = $pockpack->connect($consumer_key);
-            session(['request_token' => $request_token]);
+            $api = new PocketApi([
+                'consumerKey' => $consumer_key
+            ]);
             $url = route('pocket.auth');
-            return redirect()->away("https://getpocket.com/auth/authorize?request_token={$request_token}&redirect_uri={$url}");
+            $result = $api->requestToken($url);
+
+            $request_token = $result['request_token'];
+            session(['request_token' => $request_token]);
+            return redirect()->away($result['redirect_uri']);
         }
 
         $parent = Concept::where('parent_id', null)
@@ -55,14 +62,16 @@ class PocketController
 
     public function auth(Request $request)
     {
-        $pockpack = new PockpackAuth();
         $consumer_key = env('POCKET_CONSUMER_KEY');
+        $api = new PocketApi([
+            'consumerKey' => $consumer_key
+        ]);
         $request_token = session('request_token');
-        $access_token = $pockpack->receiveToken($consumer_key, $request_token);
+        $user = $api->convertToken($request->authorized);
         $pocket = Pocket::updateOrCreate([
             'user_id' => $request->user()->id,
         ], [
-            'access_token' => $access_token,
+            'access_token' => $user['access_token'],
         ]);
 
         return redirect()->route('pocket');
